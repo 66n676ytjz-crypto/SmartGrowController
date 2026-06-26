0391b79fb9acf64b76e12d227e6159a467706d43
66n676ytjz-crypto <66n676ytjz@privaterelay.appleid.com>
2026-06-25 23:31:36 -0400

    Update FloraController.h

--- firmware/FloraController.h
+++ firmware/FloraController.h
@@ -1,40 +1,15 @@
+/******************************************************************************
+ *
+ * Flora Core
+ * Flora Controller
+ *
+ ******************************************************************************/
+
 #pragma once
 
-#include "HAL.h"
+#include "Config.h"
 #include "GrowData.h"
 
-#include "SensorManager.h"
-#include "DisplayManager.h"
-#include "PumpManager.h"
-#include "ButtonManager.h"
-#include "BuzzerManager.h"
-
-class FloraController
-{
-public:
-
-    void begin();
-
-    void update();
-
-private:
-
-    GrowData data;
-
-    HAL hal;
-
-    SensorManager sensors{hal};
-
-    DisplayManager display;
-
-    PumpManager pump{hal};
-
-    ButtonManager button{hal};
-
-    BuzzerManager buzzer{hal};
-};#pragma once
-
-#include "GrowData.h"
 #include "HAL.h"
 
 #include "SensorManager.h"
@@ -46,22 +21,41 @@ private:
 class FloraController
 {
 public:
+
     FloraController();
 
     void begin();
+
     void update();
 
 private:
-    // Shared system data
+
+    //=========================================================
+    // Shared Data
+    //=========================================================
+
     GrowData data;
 
-    // Hardware layer
+    //=========================================================
+    // Hardware
+    //=========================================================
+
     HAL hal;
 
+    //=========================================================
     // Managers
+    //=========================================================
+
     SensorManager sensors;
+
     DisplayManager display;
+
     PumpManager pump;
+
+    ButtonManager button;
+
+    BuzzerManager buzzer;
+};
     ButtonManager button;
     BuzzerManager buzzer;
 };
