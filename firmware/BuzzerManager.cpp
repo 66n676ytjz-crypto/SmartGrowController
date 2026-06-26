b1b5eaf61c55bf3bb6daf53b84ff3f0e0f436b27
66n676ytjz-crypto <66n676ytjz@privaterelay.appleid.com>
2026-06-26 02:27:50 -0400

    Create BuzzerManager.cpp

---
+++ firmware/BuzzerManager.cpp
@@ -0,0 +1,15 @@
+#include "BuzzerManager.h"
+
+BuzzerManager::BuzzerManager(HAL& hardware)
+    : hal(hardware)
+{
+}
+
+void BuzzerManager::begin()
+{
+}
+
+void BuzzerManager::update(GrowData& data)
+{
+    // Package 3F
+}
