92868937492da61b2782de17d343fb4cdd58b4b3
66n676ytjz-crypto <66n676ytjz@privaterelay.appleid.com>
2026-06-25 23:25:34 -0400

    Update DisplayManager.h

--- firmware/DisplayManager.h
+++ firmware/DisplayManager.h
@@ -1,27 +1,47 @@
+/******************************************************************************
+ *
+ * Flora Core
+ * Display Manager
+ *
+ ******************************************************************************/
+
 #pragma once
 
+#include <Arduino.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-#include "GrowData.h"
 #include "Config.h"
+#include "GrowData.h"
 
 class DisplayManager
 {
 public:
+
+    DisplayManager();
+
     void begin();
-    void update(GrowData &data);
+
+    void update(const GrowData& data);
 
 private:
-    LiquidCrystal_I2C lcd{LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS};
 
-    bool showingInfo = false;
-    unsigned long infoStart = 0;
+    LiquidCrystal_I2C lcd;
 
-    float lastTemp = -999.0f;
-    float lastHumidity = -999.0f;
+    bool lastInfoPage = false;
+
+    float lastTemp = -1000;
+    float lastHumidity = -1000;
+
     uint8_t lastSoil = 255;
     uint8_t lastTank = 255;
 
+    void showHome(const GrowData& data);
+
+    void showInfo(const GrowData& data);
+
+    void splashScreen();
+};
     void drawHome(const GrowData &data);
     void drawInfo(const GrowData &data);
 };
