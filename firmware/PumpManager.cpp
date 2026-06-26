2ae2167c102d84573609e6ff64a52b870fc77bc8
66n676ytjz-crypto <66n676ytjz@privaterelay.appleid.com>
2026-06-26 02:26:08 -0400

    Create PumpManager.cpp

---
+++ firmware/PumpManager.cpp
@@ -0,0 +1,15 @@
+#include "PumpManager.h"
+
+PumpManager::PumpManager(HAL& hardware)
+    : hal(hardware)
+{
+}
+
+void PumpManager::begin()
+{
+}
+
+void PumpManager::update(GrowData& data)
+{
+    // Package 3E
+}
