c2c1af72644f824731efdf1dfab15b6fdf5e2df1
66n676ytjz-crypto <66n676ytjz@privaterelay.appleid.com>
2026-06-26 02:19:03 -0400

    Create SensorManager.cpp

---
+++ firmware/SensorManager.cpp
@@ -0,0 +1,15 @@
+#include "SensorManager.h"
+
+SensorManager::SensorManager(HAL& hardware)
+    : hal(hardware)
+{
+}
+
+void SensorManager::begin()
+{
+}
+
+void SensorManager::update(GrowData& data)
+{
+    // Package 3B
+}
