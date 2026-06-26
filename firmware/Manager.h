e9c50948a39020072765ef5294bf3d26ae8e5929
66n676ytjz-crypto <66n676ytjz@privaterelay.appleid.com>
2026-06-26 01:54:04 -0400

    Create Manager.h

---
+++ firmware/Manager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "GrowData.h"
+
+class Manager
+{
+public:
+    virtual ~Manager() = default;
+
+    virtual void begin() = 0;
+    virtual void update(GrowData& data) = 0;
+};
