1b5278b76138826ae0aaabf4c5439d4c0a3fe167
66n676ytjz-crypto <66n676ytjz@privaterelay.appleid.com>
2026-06-26 02:23:43 -0400

    Update ButtonManager.cpp

--- firmware/ButtonManager.cpp
+++ firmware/ButtonManager.cpp
@@ -1,14 +1,7 @@
-/******************************************************************************
- *
- * Flora Core
- * Button Manager
- *
- ******************************************************************************/
-
 #include "ButtonManager.h"
 
-ButtonManager::ButtonManager(HAL& h)
-    : hal(h)
+ButtonManager::ButtonManager(HAL& hardware)
+    : hal(hardware)
 {
 }
 
@@ -18,26 +11,5 @@ void ButtonManager::begin()
 
 void ButtonManager::update(GrowData& data)
 {
-    bool pressed = hal.readButton();
-
-    if (pressed != lastState)
-    {
-        lastDebounce = millis();
-        lastState = pressed;
-    }
-
-    if ((millis() - lastDebounce) < DEBOUNCE_MS)
-        return;
-
-    if (pressed)
-    {
-        data.display.infoPage = true;
-        data.display.pageStart = millis();
-    }
-
-    if (data.display.infoPage &&
-        millis() - data.display.pageStart > BUTTON_PAGE_TIMEOUT)
-    {
-        data.display.infoPage = false;
-    }
+    // Package 3D
 }
