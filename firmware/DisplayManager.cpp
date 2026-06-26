ac3fd3e44c4764511708241a0af6cad5ed9906a9
66n676ytjz-crypto <66n676ytjz@privaterelay.appleid.com>
2026-06-25 23:26:21 -0400

    Update DisplayManager.cpp

--- firmware/DisplayManager.cpp
+++ firmware/DisplayManager.cpp
@@ -1,71 +1,76 @@
-#pragma once
+/******************************************************************************
+ *
+ * Flora Core
+ * Display Manager
+ *
+ ******************************************************************************/
 
-#include <LiquidCrystal_I2C.h>
+#include "DisplayManager.h"
 
-#include "GrowData.h"
-#include "Config.h"
-
-class DisplayManager
+DisplayManager::DisplayManager()
+    : lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS)
 {
-public:
-    void begin();
-    void update(GrowData &data);
-
-private:
-    LiquidCrystal_I2C lcd{LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS};
-
-    bool showingInfo = false;
-    unsigned long infoStart = 0;
-
-    float lastTemp = -999.0f;
-    float lastHumidity = -999.0f;
-    uint8_t lastSoil = 255;
-    uint8_t lastTank = 255;
-
-    void drawHome(const GrowData &data);
-    void drawInfo(const GrowData &data);
-};#include "DisplayManager.h"
+}
 
 void DisplayManager::begin()
 {
+    Wire.begin(PIN_SDA, PIN_SCL);
+
     lcd.init();
+
     lcd.backlight();
 
+    splashScreen();
+}
+
+void DisplayManager::splashScreen()
+{
     lcd.clear();
+
     lcd.setCursor(0,0);
     lcd.print("Flora Core");
+
     lcd.setCursor(0,1);
     lcd.print("Starting...");
+
+    delay(1500);
+
+    lcd.clear();
 }
 
-void DisplayManager::update(GrowData &data)
+void DisplayManager::update(const GrowData& data)
 {
-    // Switch pages based on GrowData
-    showingInfo = data.display.infoPage;
-
-    if(showingInfo)
+    if(data.display.infoPage)
     {
-        if(millis() - data.display.pageStart > BUTTON_PAGE_TIMEOUT)
-        {
-            data.display.infoPage = false;
-            showingInfo = false;
-        }
+        showInfo(data);
+    }
+    else
+    {
+        showHome(data);
+    }
+}
+
+void DisplayManager::showHome(const GrowData& data)
+{
+    if(lastInfoPage)
+    {
+        lcd.clear();
+        lastInfoPage = false;
+
+        lastTemp = -1000;
+        lastHumidity = -1000;
     }
 
-    if(showingInfo)
-        drawInfo(data);
-    else
-        drawHome(data);
-}
-
-void DisplayManager::drawHome(const GrowData &data)
-{
     if(lastTemp != data.sensors.temperatureF)
     {
         lcd.setCursor(0,0);
+
         lcd.print("Temp:");
+
         lcd.print(data.sensors.temperatureF,1);
+
         lcd.print((char)223);
+
         lcd.print("F ");
 
         lastTemp = data.sensors.temperatureF;
@@ -74,21 +79,37 @@ void DisplayManager::drawHome(const GrowData &data)
     if(lastHumidity != data.sensors.humidity)
     {
         lcd.setCursor(0,1);
+
         lcd.print("Hum :");
+
         lcd.print(data.sensors.humidity,0);
+
         lcd.print("%   ");
 
         lastHumidity = data.sensors.humidity;
     }
 }
 
-void DisplayManager::drawInfo(const GrowData &data)
+void DisplayManager::showInfo(const GrowData& data)
 {
+    if(!lastInfoPage)
+    {
+        lcd.clear();
+
+        lastInfoPage = true;
+
+        lastSoil = 255;
+        lastTank = 255;
+    }
+
     if(lastSoil != data.sensors.soilPercent)
     {
         lcd.setCursor(0,0);
+
         lcd.print("Soil:");
+
         lcd.print(data.sensors.soilPercent);
+
         lcd.print("%   ");
 
         lastSoil = data.sensors.soilPercent;
@@ -97,10 +118,16 @@ void DisplayManager::drawInfo(const GrowData &data)
     if(lastTank != data.sensors.tankPercent)
     {
         lcd.setCursor(0,1);
+
         lcd.print("Tank:");
+
         lcd.print(data.sensors.tankPercent);
+
         lcd.print("%   ");
 
+        lastTank = data.sensors.tankPercent;
+    }
+}
         lastTank = data.sensors.tankPercent;
     }
 }
